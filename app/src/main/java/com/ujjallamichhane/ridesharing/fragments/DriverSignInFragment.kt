75653c952cc5c90644b540ad9a9e9aa3cf04fdce
SalinaSth <salluz2014@gmail.com>
2021-07-12 11:24:46 +0545

    Driver Sign in designed

--- app/src/main/java/com/ujjallamichhane/ridesharing/fragments/DriverSignInFragment.kt
+++ app/src/main/java/com/ujjallamichhane/ridesharing/fragments/DriverSignInFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.ujjallamichhane.ridesharing.R
 
 class DriverSignInFragment : Fragment() {
+    private lateinit var etEmailDriver: EditText
+    private lateinit var etPwdDriver: EditText
+    private lateinit var btnDriverSignIn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,10 @@ class DriverSignInFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_driver_sign_in, container, false)
+        etEmailDriver = view.findViewById(R.id.etEmailDriver)
+        etPwdDriver = view.findViewById(R.id.etPwdDriver)
+        btnDriverSignIn = view.findViewById(R.id.btnDriverSignIn)
+
         return view
     }
 
