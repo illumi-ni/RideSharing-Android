a07daab014252ac3c20ecc9218fb0dae647bfacb
SalinaSth <salluz2014@gmail.com>
2021-07-26 13:38:04 +0545

    Customer History tab layout designed

---
+++ app/src/main/java/com/ujjallamichhane/ridesharing/fragments/CustomerHistoryFragment.kt
@@ -0,0 +1,29 @@
+package com.ujjallamichhane.ridesharing.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ujjallamichhane.ridesharing.R
+
+class CustomerHistoryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_customer_history, container, false)
+
+
+        return view
+    }
+
+    companion object {
+    }
+}
