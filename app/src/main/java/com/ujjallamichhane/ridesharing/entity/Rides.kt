ade80aa25326b566c2e57d1d8a76a97b8c697818
SalinaSth <salluz2014@gmail.com>
2021-08-03 17:07:51 +0545

    Rides entity added for history

---
+++ app/src/main/java/com/ujjallamichhane/ridesharing/entity/Rides.kt
@@ -0,0 +1,11 @@
+package com.ujjallamichhane.ridesharing.entity
+
+data class Rides(
+    val _id: String? = null,
+    val date: String? = null,
+    val time: String? = null,
+    val pickup: String? = null,
+    val drop: String? = null,
+    val price: String? = null,
+    val imgCustomer: String? = null
+)
