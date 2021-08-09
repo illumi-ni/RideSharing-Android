b64f7a7a55962b2ae18d56985d8f9316e7470fe5
SalinaSth <salluz2014@gmail.com>
2021-08-09 12:20:11 +0545

    Recyclerview adapter added for rides history

--- app/src/main/java/com/ujjallamichhane/ridesharing/entity/Rides.kt
+++ app/src/main/java/com/ujjallamichhane/ridesharing/entity/Rides.kt
@@ -2,10 +2,10 @@ package com.ujjallamichhane.ridesharing.entity
 
 data class Rides(
     val _id: String? = null,
-    val date: String? = null,
-    val time: String? = null,
-    val pickup: String? = null,
-    val drop: String? = null,
-    val price: String? = null,
+    val pickupDate: String? = null,
+    val pickupTime: String? = null,
+    val pickupLocation: String? = null,
+    val dropLocation: String? = null,
+    val ridePrice: String? = null,
     val imgCustomer: String? = null
 )
