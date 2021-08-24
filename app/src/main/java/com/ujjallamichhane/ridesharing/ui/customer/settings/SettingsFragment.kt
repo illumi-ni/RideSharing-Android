63986dac31804e4b0fec48b6e7de2fb5df879a0c
SalinaSth <salluz2014@gmail.com>
2021-08-24 21:19:25 +0545

    customer logout added

--- app/src/main/java/com/ujjallamichhane/ridesharing/ui/customer/settings/SettingsFragment.kt
+++ app/src/main/java/com/ujjallamichhane/ridesharing/ui/customer/settings/SettingsFragment.kt
@@ -1,15 +1,25 @@
 package com.ujjallamichhane.ridesharing.ui.customer.settings
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.ujjallamichhane.ridesharing.R
+import com.ujjallamichhane.ridesharing.SignInActivity
 import com.ujjallamichhane.ridesharing.ui.customer.customerFragment.ProfileFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class SettingsFragment : Fragment() {
 
@@ -47,6 +57,40 @@ class SettingsFragment : Fragment() {
             }
 
 
+        tvLogout.setOnClickListener {
+            logout()
+        }
+
+
         return view
+    }
+
+    fun logout() {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Logout")
+        builder.setMessage("Are you sure you want to Log out?")
+        builder.setIcon(R.drawable.ic_logout)
+        builder.setPositiveButton("Yes") { _, _ ->
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val preferences = requireActivity().getSharedPreferences("UserPreferences", MODE_PRIVATE)
+                    val editor = preferences.edit()
+                    editor.clear()
+                    editor.apply()
+                    startActivity(Intent(context,SignInActivity::class.java)
+                    )
+                }
+                catch (ex: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(context, ex.toString(),Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        builder.setNegativeButton("No") { _, _ ->
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
